ded23ef6741b70d9e6ba9d90b0d8ad67b0278ec3
Michael Uti <utimichael9@gmail.com>
2023-09-19 19:09:00 +0100

    Init

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "My Elixir application";
+
+  inputs = {
+    beam-flakes.url = "github:shanesveller/nix-beam-flakes";
+    beam-flakes.inputs.flake-parts.follows = "flake-parts";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = inputs @ {
+    beam-flakes,
+    flake-parts,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [beam-flakes.flakeModule];
+
+      systems = ["aarch64-darwin" "x86_64-darwin" "x86_64-linux"];
+
+      perSystem = _: {
+        beamWorkspace = {
+          enable = true;
+          devShell.languageServers.elixir = true;
+          devShell.languageServers.erlang = false;
+          versions = {
+            elixir = "1.15.2";
+            erlang = "26.0.2";
+          };
+        };
+      };
+    };
+}
